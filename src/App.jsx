5f36c1a2f1d6e82ef7e307f6167a5fb343b6e0b6
sergiolfaria <srgion@yahoo.com.br>
2023-10-15 03:49:41 -0300

    Função de voltar a lista de personagens

--- src/App.jsx
+++ src/App.jsx
@@ -8,21 +8,24 @@ class App extends Component {
     characterName: null,
   };
 
-  handleCharacterClick = (name) => {
+  ClickPersonagem   = (name) => {
     this.setState({
       NomeOuDetalhe: true,
       characterName: name,
     });
   };
 
+  BotãoDeVoltar = () => {
+    this.setState({
+      NomeOuDetalhe: false,
+      characterName: null,
+    });
+  };
   render() {
     return (
       <div>
-        {this.state.NomeOuDetalhe ? (
-          <DetalhePersonagem characterName={this.state.characterName} />
-        ) : (
-          <NomePersonagem onCharacterClick={this.handleCharacterClick} />
-        )}
+        {this.state.NomeOuDetalhe ? (<button onClick={this.BotãoDeVoltar}>Voltar</button> ) : ('')}
+        {this.state.NomeOuDetalhe ? (<DetalhePersonagem characterName={this.state.characterName} /> ) : ( <NomePersonagem onCharacterClick={this.ClickPersonagem} /> )}
       </div>
     );
   }
